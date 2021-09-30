36ed39929f049aaec0da1b4630c50c792b08dc6b
Cavanha123 <89436479+Cavanha123@users.noreply.github.com>
2021-09-29 21:11:28 -0300

    Create index.js
    criação do index

---
+++ index.js
@@ -0,0 +1,11 @@
+let numerousuario;
+numerousuario = window.prompt("digite um numero")
+if(numerousuario%2==0){
+    if(numerousuario==0){
+    console.log(numerousuario)
+    }else{
+    console.log("o mumero é par:"+ numerousuario);
+    }
+    }else{
+    console.log( "o numero é impar:"+numerousuario);
+    }
